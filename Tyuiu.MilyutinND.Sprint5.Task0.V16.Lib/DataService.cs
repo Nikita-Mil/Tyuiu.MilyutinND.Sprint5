2f792280e9d099869af199d9ae9362a5a05bf080
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-11-24 19:53:12 +0500

    .

--- Tyuiu.MilyutinND.Sprint5.Task0.V16.Lib/DataService.cs
+++ Tyuiu.MilyutinND.Sprint5.Task0.V16.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MilyutinND.Sprint5.Task0.V16.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
-            double y = Math.Round((2 * Math.Pow(x, 2) - 1) / (Math.Sqrt(Math.Pow(x, 2) - 2)), 2);
+            double y = Math.Round((2 * Math.Pow(x, 2) - 1) / (Math.Sqrt(Math.Pow(x, 2) - 2)), 3);
             File.WriteAllText(path, Convert.ToString(y));
             return path;
         }
