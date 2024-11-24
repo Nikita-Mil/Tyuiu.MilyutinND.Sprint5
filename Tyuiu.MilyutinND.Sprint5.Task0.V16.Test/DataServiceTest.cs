566105e6f833be9c195c05b840397a6d9e6d05d3
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-11-24 19:43:18 +0500

    Task0

--- Tyuiu.MilyutinND.Sprint5.Task0.V16.Test/DataServiceTest.cs
+++ Tyuiu.MilyutinND.Sprint5.Task0.V16.Test/DataServiceTest.cs
@@ -8,12 +8,10 @@ namespace Tyuiu.MilyutinND.Sprint5.Task0.V16.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Asus\source\repos\Tyuiu.MilyutinND.Sprint5\Tyuiu.MilyutinND.Sprint5.Task0.V16\bin\Debug\net8.0\OutPutFileTask0.txt";
-
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-            bool wait = true;
-            Assert.AreEqual(wait, fileExists);
+            DataService ds = new DataService();
+            string path = @"C:\Users\Asus\AppData\Local\Temp\OutPutFileTask0.txt";
+            var res = ds.SaveToFileTextData(0);
+            Assert.AreEqual(path, res);
         }
     }
 }
