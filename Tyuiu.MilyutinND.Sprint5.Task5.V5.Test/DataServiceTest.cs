0e4d465fd2507dea7109af4960b2a9e5f6b82bae
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-11-26 23:05:26 +0500

    task5

--- Tyuiu.MilyutinND.Sprint5.Task5.V5.Test/DataServiceTest.cs
+++ Tyuiu.MilyutinND.Sprint5.Task5.V5.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.MilyutinND.Sprint5.Task5.V5.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\DataSprint5\InPutDataFileTask5V5.txt";
+            string path = @"C:\Users\Asus\source\repos\Tyuiu.MilyutinND.Sprint5\Tyuiu.MilyutinND.Sprint5.Task5.V5\bin\Debug\net8.0\DataSprint5\InPutDataFileTask5V5.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
