59ecfdab8dc7cabbf1e70432c9fc3c52de0649d8
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-11-26 23:37:49 +0500

    Task6

--- Tyuiu.MilyutinND.Sprint5.Task6.V5.Lib/DataService.cs
+++ Tyuiu.MilyutinND.Sprint5.Task6.V5.Lib/DataService.cs
@@ -6,7 +6,22 @@ namespace Tyuiu.MilyutinND.Sprint5.Task6.V5.Lib
     {
         public int LoadFromDataFile(string path)
         {
-            
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] >= 'A' && line[i] <= 'Z')
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
         }
     }
 }
--- Tyuiu.MilyutinND.Sprint5.Task6.V5.Test/DataServiceTest.cs
+++ Tyuiu.MilyutinND.Sprint5.Task6.V5.Test/DataServiceTest.cs
@@ -6,8 +6,25 @@ namespace Tyuiu.MilyutinND.Sprint5.Task6.V5.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalc()
         {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V5.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V5.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
